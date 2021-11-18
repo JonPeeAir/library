e83d7673abc2ee33ef7bd9be6be80f5b9250eca1
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-11-18 17:49:15 +0800

    add dummy functionality when adding a new book to the library

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
-const pagesInput = document. getElementById("pages");
+const pagesInput = document.getElementById("pages");
 
-const newBookButton = document.querySelector("button");
-newBookButton.onclick = addBookToLibrary();
+const newBookButton = document.getElementById("new-book");
+newBookButton.addEventListener("click", addBookToLibrary);
 
 let myLibrary = [];
 
 function Book(title, author, pages){
-    this.book = book;
+    this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
 function addBookToLibrary() {
-
+    alert(`Title: ${titleInput.value}, Author: ${authorInput.value}, Pages: ${pagesInput.value}`);
+    let book = new Book(titleInput.value, authorInput.value, pagesInput.value);
+    let bookElement = document.createElement("p");
+    bookElement.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}`
+    document.body.appendChild(bookElement);
 }
