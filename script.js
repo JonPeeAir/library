8b57ec17e537e63ec854ea977071c8cd695a75e7
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-11-18 15:16:37 +0800

    made a Book class and a library array to store the books

---
+++ script.js
@@ -0,0 +1,18 @@
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document. getElementById("pages");
+
+const newBookButton = document.querySelector("button");
+newBookButton.onclick = addBookToLibrary();
+
+let myLibrary = [];
+
+function Book(title, author, pages){
+    this.book = book;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+
+}
