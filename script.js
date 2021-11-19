28cbf5e5fb7ef0832b4b74359e6f01fdfa85de3e
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-11-19 13:02:35 +0800

    modal closes after user submits a book

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ function Book(title, author, pages){
 
 function addBookToLibrary() {
     alert(`Title: ${titleInput.value}, Author: ${authorInput.value}, Pages: ${pagesInput.value}`);
+    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
 
-    
+
 
     titleInput.value = "";
     authorInput.value = "";
     pagesInput.value = "";
+    closeBookModal();
 
     // This makes it so that the form doesn't actually submit the data
     return false;
@@ -41,3 +43,39 @@ function closeBookModal() {
     bookModal.classList.remove("active");
     bookModalOverlay.classList.remove("active");
 }
+
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+    alert("yes");
+  }
+  else {
+    // Too bad, no localStorage for us
+    alert("no");
+  }
+
